e00deed98997e36c3d8d2af6fa79bcf43e826fe4
Túlio Mendes <tulioalcan@gmail.com>
2022-10-04 15:24:48 -0300

    desenvolvendo requisito 06

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
 import Header from './Header';
+import Loading from './Loading';
 
 class Search extends React.Component {
   constructor() {
     super();
     this.state = {
       isButtonDisabled: true,
+      load: false,
+      artist: '',
+      albuns: [],
+      artistName: '',
     };
   }
 
@@ -27,10 +33,32 @@ class Search extends React.Component {
     }
   };
 
+  clearSearch = async () => {
+    this.setState({
+      load: true,
+    });
+    const { artist } = this.state;
+    const arrayAlbum = await searchAlbumsAPI(artist);
+    this.setState({
+      load: false,
+      albuns: arrayAlbum,
+      artist: '',
+      artistName: artist,
+    });
+  };
+
   render() {
     const {
       isButtonDisabled,
+      load,
+      albuns,
+      artistName,
     } = this.state;
+    if (load) {
+      return (
+        <Loading />
+      );
+    }
     return (
       <>
         <h1 data-testid="page-search">Conteúdo Search</h1>
@@ -50,10 +78,16 @@ class Search extends React.Component {
             type="button"
             data-testid="search-artist-button"
             disabled={ isButtonDisabled }
-            onClick={ () => {} }
+            onClick={ this.clearSearch }
           >
             Pesquisar
           </button>
+          <section>
+            {
+              albuns.length >= 1 ? <h2>{`Resultado de álbuns de: ${artistName}`}</h2>
+                : <h2>null</h2>
+            }
+          </section>
         </form>
       </>
     );
